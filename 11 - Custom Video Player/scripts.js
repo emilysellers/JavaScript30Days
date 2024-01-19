958822409b1f1bdb272e92c15ab230a7dd9386cc
Emily Sellers <emilymelindasellers@gmail.com>
2024-01-18 19:57:22 -0800

    Implement volume and playback rate range functionality

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelector(".player__slider");
+const ranges = player.querySelectorAll(".player__slider");
 
 // build functions
 function togglePlay() {
@@ -21,6 +21,10 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 // hook up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -29,3 +33,8 @@ video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
 
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mouseMove", handleRangeUpdate)
+);

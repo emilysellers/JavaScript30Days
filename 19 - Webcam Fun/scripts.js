40de4f86794c7794c9b04ea20cff1ae9ed8b4186
Emily Sellers <emilymelindasellers@gmail.com>
2024-01-31 12:09:52 -0800

    Install dependencies and stream video from webcam

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,16 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false }) // access webcam
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream;
+      video.play();
+    });
+}
+
+getVideo();

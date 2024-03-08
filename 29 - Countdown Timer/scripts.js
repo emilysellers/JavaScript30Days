b6bc4fbd8595d0467a4cc05dfac7f5d3254de290
Emily Sellers <emilymelindasellers@gmail.com>
2024-03-08 13:48:14 -0800

    Connect buttons to timer function

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -2,6 +2,7 @@ let countdown;
 
 const timerDisplay = document.querySelector(".display__time-left");
 const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
 function timer(seconds) {
   const now = Date.now();
@@ -33,7 +34,14 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minute = end.getMinutes();
-  endTime.textContent = `We're back at ${
-    hour > 12 ? hour - 12 : hour
-  }:${minute}.`;
+  endTime.textContent = `We're back at ${hour > 12 ? hour - 12 : hour}:${
+    minute < 10 ? "0" : ""
+  }${minute}.`;
 }
+
+function startTimer() {
+  buttonTime = parseInt(this.dataset.time);
+  timer(buttonTime);
+}
+
+buttons.forEach((button) => button.addEventListener("click", startTimer));
